bdb2fc440ff332a8bd806dc9857abcf564da3d28
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-11-29 23:53:54 +0100

    J'ai apporté des modifications au code afin d'améliorer sa structure.

--- E_Bank_Solution/src/main/java/com/example/E_Bank_Solution/Controller/TransactionController.java
+++ E_Bank_Solution/src/main/java/com/example/E_Bank_Solution/Controller/TransactionController.java
@@ -3,23 +3,29 @@ package com.example.E_Bank_Solution.Controller;
 import com.example.E_Bank_Solution.Model.Transaction;
 import com.example.E_Bank_Solution.Service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.pulsar.PulsarProperties;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
+/**
+ * Contrôleur pour la gestion des transactions dans la solution E-Bank.
+ */
 @RestController
 public class TransactionController {
-@Autowired
-private TransactionService transactionService;
-    @PostMapping("/transaction/{idCompte}")
-    public String addTransaction(@PathVariable Long idCompte, @RequestBody Transaction transaction){
-        return transactionService.addTransaction(idCompte,transaction);
-    }
 
+    @Autowired
+    private TransactionService transactionService;
+
+    /**
+     * Effectue une transaction sur un compte spécifié.
+     *
+     * @param idCompte   l'identifiant du compte sur lequel la transaction est effectuée
+     * @param transaction l'objet Transaction contenant les détails de la transaction
+     * @return un message indiquant le résultat de l'opération
+     */
+    @PostMapping("/transaction/{idCompte}")
+    public String addTransaction(@PathVariable Long idCompte, @RequestBody Transaction transaction) {
+        return transactionService.addTransaction(idCompte, transaction);
+    }
 }
--- E_Bank_Solution/src/main/java/com/example/E_Bank_Solution/Controller/UserController.java
+++ E_Bank_Solution/src/main/java/com/example/E_Bank_Solution/Controller/UserController.java
@@ -1,14 +1,15 @@
 package com.example.E_Bank_Solution.Controller;
 
-
 import com.example.E_Bank_Solution.Model.User;
 import com.example.E_Bank_Solution.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Contrôleur pour la gestion des utilisateurs dans la solution E-Bank.
+ */
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/user")
@@ -17,21 +18,42 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * Récupère la liste de tous les utilisateurs.
+     *
+     * @return une liste d'utilisateurs
+     */
     @GetMapping("/getAll")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    /**
+     * Ajoute un nouvel utilisateur.
+     *
+     * @param user l'utilisateur à ajouter
+     */
     @PostMapping("/addUser")
     public void addUser(@RequestBody User user) {
         userService.saveUser(user);
     }
 
+    /**
+     * Supprime un utilisateur par son identifiant.
+     *
+     * @param id l'identifiant de l'utilisateur à supprimer
+     */
     @DeleteMapping("/delete/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
 
+    /**
+     * Met à jour les informations d'un utilisateur existant.
+     *
+     * @param id   l'identifiant de l'utilisateur à mettre à jour
+     * @param user l'objet User contenant les nouvelles informations
+     */
     @PutMapping("/update/{id}")
     public void updateUser(@PathVariable Long id, @RequestBody User user) {
         userService.updateUser(id, user);
